0c34aa6dfba8619f52af08df431e576789a71c38
Alexey Troyanovskiy <shtyr111@gmail.com>
2022-01-07 19:42:43 +0300

    commi

---
+++ src/main/java/com/alex/test_kata_3_1_1/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.alex.test_kata_3_1_1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "test";
+    }
+}
